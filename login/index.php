f41df7179e6842f7ff2325258281ba487e9f3140
andrespatino998 <78108699+andrespatino998@users.noreply.github.com>
2022-03-13 13:06:55 -0600

    s

--- login/index.php
+++ login/index.php
@@ -43,7 +43,7 @@
          </div>         
      </div>        
          
-     <script src="jquery/jquery-3.3.1.min.js"></script>    
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>    
      <script src="bootstrap/js/bootstrap.min.js"></script>    
      <script src="popper/popper.min.js"></script>    
         
