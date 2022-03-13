f986e7527e899f1d7df509c481c458b6214fec11
andrespatino998 <78108699+andrespatino998@users.noreply.github.com>
2022-03-13 13:58:54 -0600

    asd

--- login/dashboard/vistas/parte_inferior.php
+++ login/dashboard/vistas/parte_inferior.php
@@ -43,11 +43,11 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="js/sb-admin-2.min.js"></script>
